e58a03c888c6510f2c7fd344c577a1987b776d26
dyegohrq <dyegoh408@gmail.com>
2026-04-23 10:17:02 -0300

    Pagina home finalizada

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
 // trabalhando no login
 
 // FInalizando sistema de login
+
+function login(nome, email) {
+    return "Bem vindo"
+}
+
+// Sistema d elogin completo
+// Retornamos o nome do usuario quando logado
